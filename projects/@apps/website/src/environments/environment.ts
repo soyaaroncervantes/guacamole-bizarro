b8dd7bad2beb52a446be82dcbe461c937f451dd1
Aarón Cervantes Álvarez <soy.aaron.cervantes@gmail.com>
2020-09-22 00:03:56 -0500

    Erased config

--- projects/@apps/website/src/environments/environment.ts
+++ projects/@apps/website/src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyA7fIL3jqR40tQk1xs1G2ztrN49kWdH-ik',
-    authDomain: 'albums-monorepo.firebaseapp.com',
-    databaseURL: 'https://albums-monorepo.firebaseio.com',
-    projectId: 'albums-monorepo',
-    storageBucket: 'albums-monorepo.appspot.com',
-    messagingSenderId: '828547260239',
-    appId: '1:828547260239:web:124c1caa9c9ca0c6dc6f3a',
-    measurementId: 'G-XKB3BRZTZK'
-  }
 };
 
 /*
