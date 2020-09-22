895a1417ad53cd965f29ea7a16577680068184dc
Aarón Cervantes Álvarez <soy.aaron.cervantes@gmail.com>
2020-09-21 22:55:55 -0500

    Added new app & config lib

---
+++ projects/config/firebase/src/lib/firebase.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-firebase',
+  template: `
+    <p>
+      firebase works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FirebaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/config/firebase/src/lib/firebase.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FirebaseComponent } from './firebase.component';
+
+
+
+@NgModule({
+  declarations: [FirebaseComponent],
+  imports: [
+  ],
+  exports: [FirebaseComponent]
+})
+export class FirebaseModule { }
