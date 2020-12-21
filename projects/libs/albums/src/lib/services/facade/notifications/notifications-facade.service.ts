f2bacfb74f0e11c7a49a0508dd1f69f98d8bdc1e
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-12-21 08:29:40 -0600

    Erased previous .run files and added new ones

--- projects/libs/albums/src/lib/services/facade/notifications/notifications-facade.service.ts
+++ projects/libs/albums/src/lib/services/facade/notifications/notifications-facade.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarRef } from '@angular/material/snack-bar/snack-bar-ref';
+import { TextOnlySnackBar } from '@angular/material/snack-bar/simple-snack-bar';
 
 @Injectable({
   providedIn: 'platform'
@@ -10,15 +12,15 @@ export class NotificationsFacadeService {
     private matSnackBar: MatSnackBar
   ) { }
 
-  addAlbum(): void {
-    this.matSnackBar.open('Added a new Album!');
+  get addAlbum(): MatSnackBarRef< TextOnlySnackBar > {
+    return this.matSnackBar.open('Added a new Album!');
   }
 
-  editAlbum(): void {
-    this.matSnackBar.open('Edited an Album!');
+  get editAlbum(): MatSnackBarRef< TextOnlySnackBar > {
+    return this.matSnackBar.open('Edited an Album!');
   }
 
-  deleteAlbum(): void {
-    this.matSnackBar.open('Deleted an Album!');
+  get deleteAlbum(): MatSnackBarRef< TextOnlySnackBar > {
+    return this.matSnackBar.open('Deleted an Album!');
   }
 }
