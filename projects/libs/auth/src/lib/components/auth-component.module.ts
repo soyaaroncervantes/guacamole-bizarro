d9de6342fd0baa40904de83086d759927a3958d5
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-12-26 20:05:01 -0600

    Erased services, and added new buttons

--- projects/libs/auth/src/lib/components/auth-component.module.ts
+++ projects/libs/auth/src/lib/components/auth-component.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GoogleButtonComponent } from './button/google/google-button.component';
+import { TwitterButtonComponent } from './button/twitter/twitter-button.component';
+import { FacebookButtonComponent } from './button/facebook/facebook-button.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GoogleButtonComponent,
+    TwitterButtonComponent,
+    FacebookButtonComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    GoogleButtonComponent,
+    TwitterButtonComponent,
+    FacebookButtonComponent
   ]
 })
 export class AuthComponentModule { }
---
+++ projects/libs/auth/src/lib/components/button/google/google-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auth-google-button',
+  templateUrl: './google-button.component.html',
+  styleUrls: ['./google-button.component.scss']
+})
+export class GoogleButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
