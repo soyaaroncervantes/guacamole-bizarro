b27cd10ffb2caf6de2f1867b4e09af18089724c8
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-12-28 00:08:57 -0600

    Added auth and store module

--- projects/apps/albums/src/app/app.module.ts
+++ projects/apps/albums/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AuthModule } from '@libs/auth';
+
 import { AppRoutingModule } from './app-routing.module';
+import { AppStoreModule } from './app-store.module';
+
 import { AppComponent } from './app.component';
-import { AuthComponentModule } from '@libs/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AuthModule,
+    AngularFireModule.initializeApp( environment ),
     AppRoutingModule,
-    AuthComponentModule,
-    BrowserModule
+    AppStoreModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
