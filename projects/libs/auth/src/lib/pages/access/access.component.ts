1fcd44015decdc1d2f263a81ba64987e8b5ae74c
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-12-31 01:00:01 -0600

    Created pages folder

---
+++ projects/libs/auth/src/lib/pages/access/access.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auth-access',
+  templateUrl: './access.component.html',
+  styleUrls: ['./access.component.scss']
+})
+export class AccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
