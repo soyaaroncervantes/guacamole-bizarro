55ce7e646971cceb3a6aa51d41078890dd27ad66
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-12-31 00:54:30 -0600

    Modiifed albums app

--- projects/apps/albums/src/app/app-routing.module.ts
+++ projects/apps/albums/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'access', loadChildren: () => import('@libs/auth').then( m => m.AccessModule ) },
+  { path: '', redirectTo: 'access', pathMatch: 'full' },
+  { path: '**', redirectTo: 'access', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/apps/albums/src/app/app.module.ts
+++ projects/apps/albums/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppStoreModule } from './app-store.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,10 +17,11 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     AuthModule,
-    AngularFireModule.initializeApp( environment ),
+    AngularFireModule.initializeApp( environment.firebase ),
     AppRoutingModule,
     AppStoreModule,
     BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
